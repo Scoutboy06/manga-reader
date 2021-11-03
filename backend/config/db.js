6b1c69b2c48aea126415a555953799f564455126
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2021-11-03 20:08:17 +0100

    Removed environmental variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.local' });
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb+srv://Elias:5gu!B%409SV!@cluster0.xo75c.mongodb.net/manga_reader_db?retryWrites=true&w=majority&keepAlive=true&socketTimeoutMS=360000&connectTimeoutMS=360000' , { // process.env.MONGO_URI
       useUnifiedTopology: true,
       useNewUrlParser: true,
       // useCreateIndex: true,
