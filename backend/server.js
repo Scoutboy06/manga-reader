a3fc10ea36d7d34e83d1c54bcdac25afa9f2f7d0
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2021-12-12 21:01:14 +0100

    Using single port on production

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import path from 'path';
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -13,13 +13,12 @@ import singleRoutes from './routes/singleRoutes.js';
 import { getSubscribedUpdates } from './controllers/updatesController.js';
 
 
-// dotenv.config({ path: '.env.local' });
+const __dirname = path.resolve();
 dotenv.config({ path: '.env' });
 
 connectDB();
 
 const app = express();
-// const router = express.Router();
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -34,19 +33,9 @@ app.use('/api/image', imageRoutes);
 app.get('/api/getUpdates', getSubscribedUpdates);
 
 
-// const __dirname = path.resolve();
-
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-// app.use(express.static(path.join(__dirname, 'client')));
-
-// app.get('/', (req, res) =>
-//   res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
-// );
-
-// app.get('/:urlName/:chapter', (req, res) =>
-//   res.sendFile(path.resolve(__dirname, 'client', 'page.html'))
-// );
+if(process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'build')));
+}
 
 
 app.use(notFound);
@@ -54,7 +43,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-const PORT = process.env.BACKEND_PORT;
-app.listen(PORT,
-  console.log(`Server running on http://127.0.0.1:${PORT}`),
-);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on http://127.0.0.1:${PORT}`);
+});
