cbfbd1600a5a0cd2aeac691b8e187ecae2cbae49
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2021-12-21 21:45:10 +0100

    Bug when not starting at /

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.use('/', express.static(path.join(__dirname, 'build')));
 }
 
+app.use('*', express.static(path.join(__dirname, 'build/index.html')));
+
 // else if(process.env.NODE_ENV === 'development') {
 //   app.use('/', createProxyMiddleware({ target: `127.0.0.1:${process.env.PORT}/` }));
 // }
