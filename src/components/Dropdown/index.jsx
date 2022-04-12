630e7050a1a89d428d181a8b7bf8e43c00f697a7
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2022-04-12 22:14:20 +0200

    Library responsiveness

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from 'react';
+
+import styles from '../ContextMenu/ContextMenu.module.css';
+
+export default function ContextMenu({ items, isShown, pos }) {
+	return (
+		<div
+			className={styles.dropdown}
+			data-isshown={isShown}
+			style={{
+				left: pos.x,
+				top: pos.y,
+			}}
+			onClick={e => e.preventDefault()}
+		>
+			{items.map((item, index) => {
+				if (item === null) return null;
+
+				if (item === 'divider') {
+					return <div key={index} className={styles.divider}></div>;
+				}
+
+				return (
+					<div
+						key={index}
+						onClick={() => {
+							if (item.action && !item.disabled) item.action();
+						}}
+						className={styles.item}
+						disabled={item?.disabled ? true : false}
+					>
+						{item.icon && <div className={styles.icon}>{item.icon}</div>}
+						<span className={styles.text}>{item?.content || item}</span>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
