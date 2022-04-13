c652bcc0c932be18a083ec4d4430a4d3b306ac44
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2022-04-13 13:42:42 +0200

    Remove console.log

--- backend/functions/chapterNameToChapter.js
+++ backend/functions/chapterNameToChapter.js
@@ -1,6 +1,5 @@
 export default function chapterNameToChapter(name) {
 	const regex = new RegExp('(.+) ?- ?(chapter.+)', 'gi');
 	const match = regex.exec(name);
-	console.log(match);
 	return match[2];
 }
