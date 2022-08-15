777ee56a2c290b77a2b1505e11126fe30dc48836
Scoutboy06 <32744805+Scoutboy06@users.noreply.github.com>
2022-08-15 21:51:08 +0200

    Popup rework

--- client/src/components/PopupCreator/index.jsx
+++ client/src/components/PopupCreator/index.jsx
@@ -22,7 +22,7 @@ export default function PopupCreator() {
 		<div className={styles.blackContainer} data-show={showBlackScreen}>
 			{popups.map((popup, popupIndex) => (
 				<div
-					className={styles.container}
+					className={styles.popupContainer}
 					key={popupIndex}
 					data-show={
 						popup.isVisible &&
@@ -36,17 +36,11 @@ export default function PopupCreator() {
 							className={styles.closeBtn}
 							onClick={() => actions.closePopup(popupIndex)}
 						>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 24 24'
-								fill='#888'
-							>
-								<path d='M0 0h24v24H0V0z' fill='none' />
-								<path d='M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z' />
-							</svg>
+							<i className='icon'>close</i>
 						</button>
 					</div>
-					<div className={styles.content}>
+
+					<div className={styles.body}>
 						<popup.content
 							closePopup={() => actions.closePopup(popupIndex)}
 							data={popup.data}
--- client/src/pages/library/index.jsx
+++ client/src/pages/library/index.jsx
@@ -1,26 +1,12 @@
-import { useState, useContext } from 'react';
-import { useNavigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import Dropdown from '../../components/Dropdown';
-import BlurContainer from '../../components/BlurContainer';
 import Navbar from '../../components/navbars/Library';
 
-import { ProfileContext } from '../../contexts/ProfileContext';
-
-import styles from './index.module.css';
-
 export default function Library() {
-	const navigate = useNavigate();
-	const [profileData, profileActions] = useContext(ProfileContext);
-
-	const [showProfileDropdown, setShowProfileDropdown] = useState(false);
-
 	return (
 		<>
-			<main className={styles.main}>
-				<Navbar />
-				<Outlet />
-			</main>
+			<Navbar />
+			<Outlet />
 		</>
 	);
 }
